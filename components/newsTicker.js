a5e5c90439d83dbfc437888b8a431d1f92cc6057
MJ <josco.michael@gmail.com>
2024-12-31 17:15:38 -0600

    add hyperlinks to news ticker
    news ticker headlines with real studies now have a link to those studies

--- components/newsTicker.js
+++ components/newsTicker.js
@@ -2,28 +2,40 @@ function queueNewsTicker(value){
 	gameData.newsTickerNext.push(value);
 }
 
-function updateNewsTicker(){
+function updateNewsTicker() {
 
+	// Get the next queued news, if there is one
+	let newsEntry;
 	if (gameData.newsTickerNext.length >= 1) {
 		next = gameData.newsTickerNext.shift();
 	}
+	// otherwise, if enough time has passed, grab a random one from the list
 	else if (gameData.timer - gameData.newsTickerTimeAtLastUpdate > 1000) {
-		next = news[Math.floor(Math.random()*news.length)]
+		newsEntry = Math.floor(Math.random()*news.length);
+		next = news[newsEntry];
 	}
 	else {
 		return;
 	}
-	console.log(next);
+	
+	let text = next[0];
+	let tickerHTML;
+	if (next.length == 1 || next[1] == "") {
+		tickerHTML = text;
+	} else {
+		tickerHTML = `<a href="${next[1]}" target="_blank">${text} </a><sup>[<a href="${next[1]}">${newsEntry}</a>]</sup>`
+	}
 	gameData.newsTickerTimeAtLastUpdate = gameData.timer;
-	gameData.NewsTickerText = next;
-	document.getElementById("tickerText").innerHTML = next;
+	gameData.NewsTickerText = tickerHTML;
+	document.getElementById("tickerText").innerHTML = tickerHTML;
 }
 
 var news = [
-	"Advocates Call for More Bike Paths",
-	"Research Shows Bikes are Good For You",
-	"Study Shows Bikes are Faster for Inner City Delivery",
-	"Scientists Discover You Should Bike More",
-	"Researchers find no connection between bicycles and bigfoot sightings",
-	"Opinion: Patriots Say More Bikes = More Freedom",
-	]
+	["Advocates Call for More Bike Paths", ""],
+	["Research Shows Bikes are Good For You", "https://www.betterhealth.vic.gov.au/health/healthyliving/cycling-health-benefits"],
+	["Study Suggests Bikes are Faster for Inner City Delivery", "https://www.larryvsharry.com/media/wysiwyg/cms_pages/Stories/Last_Mile_Delivery/Data-driven_Evaluation_of_Cargo_Bike_Delivery_Performance_in_Brussels.pdf"],
+	["Scientists Discover You Should Bike More", ""],
+	["Researchers find no connection between bicycles and bigfoot sightings", ""],
+	["Opinion: Patriots Say More Bikes = More Freedom", ""],
+	["Urbanists: E-bikes are fastest way to the airport", "https://www.wsj.com/lifestyle/travel/airport-race-new-york-chicago-los-angeles-67a2acbf"],
+]
